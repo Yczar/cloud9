9c6b27ef4b199c74c1df447c9fbef298cef64841
Yczar <babalolagbogo@gmail.com>
2022-12-30 00:18:42 +0100

    delete: main.dart file

--- example/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:cloud9/cloud9.dart';
-
-void main() {
-  final cloud9 = Cloud9.instance;
-  final initializer = GoogleDriveInitializer()..initialize();
-  final connector = GoogleDriveConnector()..connect();
-}
