be635a3ccede80b66308c8ef538fbbce8f30ce3e
Yczar <babalolagbogo@gmail.com>
2022-12-30 16:55:46 +0100

    chore: create googledrive connector

---
+++ lib/src/connectors/src/implementation/googledrive_connector.dart
@@ -0,0 +1,9 @@
+import 'package:cloud9/src/connectors/src/cloud9_connector.dart';
+
+/// An implementation of the `Cloud9Connector` abstract class for Google Drive.
+class GoogleDriveConnector implements Cloud9Connector {
+  @override
+  Future<void> connect() async {
+    // Connect to Google Drive here.
+  }
+}
