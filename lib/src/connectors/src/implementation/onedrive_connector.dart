6a13dd87cdf1230ad030bb2f601f03f8956d609a
Yczar <babalolagbogo@gmail.com>
2022-12-30 16:55:16 +0100

    chore: create onedrive connector

---
+++ lib/src/connectors/src/implementation/onedrive_connector.dart
@@ -0,0 +1,10 @@
+import 'package:cloud9/src/connectors/src/cloud9_connector.dart';
+
+/// An implementation of the `Cloud9Connector` abstract class for Microsoft
+/// OneDrive.
+class OneDriveConnector implements Cloud9Connector {
+  @override
+  Future<void> connect() async {
+    // Connect to OneDrive here.
+  }
+}
