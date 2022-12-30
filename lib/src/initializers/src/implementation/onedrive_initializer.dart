9f1a8b6006ea831135377d13287f256226d5d7aa
Yczar <babalolagbogo@gmail.com>
2022-12-30 16:54:11 +0100

    chore: create onedrive initializer

---
+++ lib/src/initializers/src/implementation/onedrive_initializer.dart
@@ -0,0 +1,10 @@
+import 'package:cloud9/src/initializers/src/cloud9_initializer.dart';
+
+/// An implementation of the `Cloud9Initializer` abstract class for Microsoft
+/// OneDrive.
+class OneDriveInitializer implements Cloud9Initializer {
+  @override
+  void initialize() {
+    // Initialize OneDrive here.
+  }
+}
