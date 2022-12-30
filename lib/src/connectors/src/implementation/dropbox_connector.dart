7da22286eac84f80c4f0bcb5ca9370f03e84a133
Yczar <babalolagbogo@gmail.com>
2022-12-30 16:55:36 +0100

    choe: create dropbox connector

---
+++ lib/src/connectors/src/implementation/dropbox_connector.dart
@@ -0,0 +1,9 @@
+import 'package:cloud9/src/connectors/src/cloud9_connector.dart';
+
+/// An implementation of the `Cloud9Connector` abstract class for Dropbox.
+class DropBoxConnector implements Cloud9Connector {
+  @override
+  Future<void> connect() async {
+    // Connect to Dropbox here.
+  }
+}
