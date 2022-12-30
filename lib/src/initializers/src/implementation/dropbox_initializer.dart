b05d5494a642807ee46e1a5d233969dcd1e7cb4d
Yczar <babalolagbogo@gmail.com>
2022-12-30 16:54:38 +0100

    chore: create drop box initializer

---
+++ lib/src/initializers/src/implementation/dropbox_initializer.dart
@@ -0,0 +1,9 @@
+import 'package:cloud9/src/initializers/src/cloud9_initializer.dart';
+
+/// An implementation of the `Cloud9Initializer` abstract class for Dropbox.
+class DropBoxInitializer implements Cloud9Initializer {
+  @override
+  void initialize() {
+    // Initialize Dropbox here.
+  }
+}
